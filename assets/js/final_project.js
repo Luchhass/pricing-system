777c0a198db0cfc636b5b74c3885d3488776a630
Furkan Coşar <furkancosar2005@gmail.com>
2025-07-25 06:45:33 +0300

    css fixes

--- assets/js/final_project.js
+++ assets/js/final_project.js
@@ -1,40 +1,70 @@
-let pricing = [];
+let pricing = {
+  heading: "Simple, traffic-based pricing",
+  subheading: "Sign-up for our 30-day trial. No credit card required.",
+  billingCycle: {
+    monthly: {
+      name: "Monthly Billing",
+    },
+    yearly: {
+      name: "Yearly Billing",
+    },
+  },
+  features: ["Unlimited websites", "100% data ownership", "Email reports"],
+};
 
-function renderPricing(price) {
-    headerText.innerText = pricing.heading;
-    subheadingText.innerText = pricing.subheading;
-    productNameText.innerText = pricing.productName;
-    monthlyBillingText.innerText = pricing.billingCycle.monthly.name;
-    yearlyBillingText.innerHTML = `${pricing.billingCycle.yearly.name}&nbsp;&nbsp;&nbsp;&nbsp;<span>-25%</span>`;
-    unlimitedText.innerHTML = `<img src="./assets/img/features_tick_logo.png" alt=""> ${pricing.features[0]}`;
-    dataText.innerHTML = `<img src="./assets/img/features_tick_logo.png" alt=""> ${pricing.features[1]}`;
-    EmailText.innerHTML = `<img src="./assets/img/features_tick_logo.png" alt=""> ${pricing.features[2]}`;
+function renderPricing() {
+  headerText.innerText = pricing.heading;
+  subheadingText.innerText = pricing.subheading;
+  monthlyBillingText.innerText = pricing.billingCycle.monthly.name;
+  yearlyBillingText.innerHTML = `${pricing.billingCycle.yearly.name}&nbsp;&nbsp;&nbsp;&nbsp;<span>-25%</span>`;
+  unlimitedText.innerHTML = `<img src="./assets/img/features_tick_logo.png" alt=""> ${pricing.features[0]}`;
+  dataText.innerHTML = `<img src="./assets/img/features_tick_logo.png" alt=""> ${pricing.features[1]}`;
+  EmailText.innerHTML = `<img src="./assets/img/features_tick_logo.png" alt=""> ${pricing.features[2]}`;
 }
 
-console.log(finalPrice);
-
-function finalPricing() {
-    if(checkbox.checked) {
-        finalPrice.innerText = `$${((((200000 / Number(rangeAmount.value)) * 3.2) / 100)*75).toFixed(2)}`;
-        monthOrYear.innerText = `/ year`;
-    } else {
-        finalPrice.innerText = `$ ${(((200000 / Number(rangeAmount.value)) * 3.2).toFixed(2))} `;
-        monthOrYear.innerText = `/ month`;
-    }
-    console.log(rangeAmount.value);
-    console.log(200000 / Number(rangeAmount.value));
-    console.log(((200000/ Number(rangeAmount.value)) * 3.2));
+// Sayfa görüntüleme metnini güncelle (örnek: 100K PAGEVIEWS)
+function updatePageViews(value) {
+  let displayText;
+  if (value >= 1000 && value < 1000000) {
+    displayText = `${value / 1000}K PAGEVIEWS`;
+  } else if (value >= 1000000) {
+    displayText = `${value / 1000000}M PAGEVIEWS`;
+  } else {
+    displayText = `${value} PAGEVIEWS`;
+  }
+  productNameText.innerText = displayText;
 }
 
-myTrial.addEventListener('click', finalPricing);
+// Fiyatı hesapla
+function updatePrice(value) {
+  const basePrice = ((value / 1000) * 2).toFixed(2); // örnek hesap: 100K = 200.00$
+  if (checkbox.checked) {
+    finalPrice.innerText = `$${(basePrice * 0.75).toFixed(2)}`;
+    monthOrYear.innerText = "/ year";
+  } else {
+    finalPrice.innerText = `$${basePrice}`;
+    monthOrYear.innerText = "/ month";
+  }
+}
 
+// Hem pageview hem fiyatı güncelle
+function handleSliderChange() {
+  const value = parseInt(rangeAmount.value);
+  updatePageViews(value);
+  updatePrice(value);
+}
+
+// Başlangıçta çalıştır
 function init() {
-    fetch("https://dummyjson.czaylabs.com.tr/api/exam/interactive-pricing")
-    .then((res) => res.json())
-    .then((res) => {
-        pricing = res;
-        renderPricing();
-    })
+  renderPricing();
+  handleSliderChange(); // İlk değer için çalıştır
 }
+
+// Event listener’lar
+rangeAmount.addEventListener("input", handleSliderChange);
+checkbox.addEventListener("change", () =>
+  updatePrice(parseInt(rangeAmount.value))
+);
+myTrial.addEventListener("click", handleSliderChange);
 
 init();
